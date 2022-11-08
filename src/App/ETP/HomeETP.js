256a1a7fba7798d685ce336daf20fe5635376ca2
AlexiaTdev <alexia.teissier1@epsi.fr>
2022-11-08 10:30:46 +0100

    init home pages

---
+++ src/App/ETP/HomeETP.js
@@ -0,0 +1,12 @@
+import PropTypes from 'prop-types'
+import React, { PureComponent } from 'react'
+
+export default class HomeETP extends PureComponent {
+  static propTypes = {}
+
+  render() {
+    return (
+      <div>HomeETP</div>
+    )
+  }
+}
