5ebe1d29329bb058b6bb272ca3ddba1e199dac0f
wilfriedndr <wilfried.ndr@gmail.com>
2022-11-09 16:33:48 +0100

    s

--- src/MarketPage/AboutusCpnt/Aboutus.js
+++ src/MarketPage/AboutusCpnt/Aboutus.js
@@ -8,9 +8,10 @@ export class Aboutus extends PureComponent {
   static propTypes = {}
 
   async componentDidMount() {
-    console.log('-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------')
+    console.log('componentDidMount')
     // GET request using fetch with async/await
-    const response = await fetch('http://localhost:3000/api/user');
+    console.log("-------------------------------------------------------------------------------------------------------------------------------------------------------")
+    const response = await fetch('https://www.ecochauffage.guitteny.net:3000/api/user', {mode:'cors'});
     const data = await response.json();
     this.setState({ totalReactPackages: data.total })
     console.log("ICIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII");
