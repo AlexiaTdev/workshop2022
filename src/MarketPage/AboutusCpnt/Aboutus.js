77c9728b8c19ae0229bbe558042d6a01ef8c9063
wilfriedndr <wilfried.ndr@gmail.com>
2022-11-09 16:52:08 +0100

    s

--- src/MarketPage/AboutusCpnt/Aboutus.js
+++ src/MarketPage/AboutusCpnt/Aboutus.js
@@ -8,14 +8,34 @@ export class Aboutus extends PureComponent {
   static propTypes = {}
 
   async componentDidMount() {
-    console.log('componentDidMount')
     // GET request using fetch with async/await
     console.log("-------------------------------------------------------------------------------------------------------------------------------------------------------")
-    const response = await fetch('https://www.ecochauffage.guitteny.net:3000/api/user', {mode:'cors'});
-    const data = await response.json();
-    this.setState({ totalReactPackages: data.total })
-    console.log("ICIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII");
-    console.log(data);
+    // const response = await fetch('https://www.ecochauffage.guitteny.net:3000/api/user', {mode:'cors'});
+    // const data = await response.json();
+
+    // GET request using fetch with error handling
+    fetch('https://www.ecochauffage.guitteny.net:3000/api/user')
+      .then(async response => {
+        console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+        const data = await response.json();
+        console.log("-------------------------------------------------------------------------------------------------------------------------------------------------------")
+        // check for error response
+        if (!response.ok) {
+          // get error message from body or default to response statusText
+          const error = (data && data.message) || response.statusText;
+          return Promise.reject(error);
+        }
+        console.log("ICIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII");
+        this.setState({ totalReactPackages: data.total })
+      })
+      .catch(error => {
+        this.setState({ errorMessage: error.toString() });
+        console.error('There was an error!', error);
+      });
+
+    // this.setState({ totalReactPackages: data.total })
+    // console.log("ICIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII");
+    // console.log(data);
   };
 
   render() {
